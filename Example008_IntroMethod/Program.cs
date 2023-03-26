7609ecb1ab26c805310a9015b05a42fdfbd490f6
Barys Smaliak <smboris@yandex.ru>
2023-03-26 10:06:24 +0300

    function to example008 added

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 23;
+int a2 = 18;
+int a3 = 43;
+int b1 = 18;
+int b2 = 26;
+int b3 = 3;
+int c1 = 56;
+int c2 = 78;
+int c3 = 34;
