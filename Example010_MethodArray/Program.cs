506dd3128802b5143d43885e362e5801ec21386d
Barys Smaliak <smboris@yandex.ru>
2023-03-26 13:12:02 +0300

    Example010 added

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {18, 21, 13, 4, 25, 4, 66, 74};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
