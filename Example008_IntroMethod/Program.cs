8adc8aeb67c4bb494e548fe5200b4bf36608581f
Barys Smaliak <smboris@yandex.ru>
2023-03-26 10:20:28 +0300

    example008 finished

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -12,7 +12,17 @@ int a2 = 18;
 int a3 = 43;
 int b1 = 18;
 int b2 = 26;
-int b3 = 3;
+int b3 = 312;
 int c1 = 56;
 int c2 = 78;
-int c3 = 34;
+int c3 = 36;
+
+
+//int max1 = Max(a1, a2, a3);
+//int max2 = Max(b1, b2, b3);
+//int max3 = Max(c1, c2, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, a2, a3), Max(b1, b2, b3), Max(c1, c2, c3));
+
+Console.WriteLine(max);
